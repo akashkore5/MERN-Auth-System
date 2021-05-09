47dc2ac0e8be405b2f356f9176a7460b89e28e2d
Harshal Deshpande <harshal25012001@gmail.com>
2021-05-09 10:08:34 +0530

    Version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://Admin:Hprachi@cluster0-shard-00-00.l9h47.mongodb.net:27017,cluster0-shard-00-01.l9h47.mongodb.net:27017,cluster0-shard-00-02.l9h47.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
